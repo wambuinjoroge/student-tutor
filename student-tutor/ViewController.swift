f9a57d709756e1560071002cc2270c2fb7652ebd
Yvonne ariga <yvonnekemi@hotmail.com>
2019-07-30 09:30:50 +0300

    initialized repo

---
+++ student-tutor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  student-tutor
+//
+//  Created by MobiDev on 30/07/2019.
+//  Copyright © 2019 MobiDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
